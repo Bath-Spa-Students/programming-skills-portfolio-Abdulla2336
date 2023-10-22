c46327bc370241b2117f92ec8ecfbdaafef4f425
Abdulla2336 <144320630+Abdulla2336@users.noreply.github.com>
2023-10-23 00:09:40 +0400

    exercise 4.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 4.py
@@ -0,0 +1,13 @@
+#If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d
+#like to invite to dinner. Then use your list to print a message to each person, invitingthem to dinner
+
+guests = ['abood', 'hamood', 'sahood']
+
+name = guests[0].title()
+print(name + ", would u join for dinner tonight.")
+
+name = guests[1].title()
+print(name + ", would u join for dinner tonight.")
+
+name = guests[2].title()
+print(name + ", would u join for dinner tonight.")
